51176c99c67d49000c491f234a5e255f7d0e6b80
fomes <fgomesdeluna@gmail.com>
2023-10-06 11:44:26 -0300

    Fix next warn, Split nav components

---
+++ src/components/NavDesktop/index.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+import { NavItem } from "../NavItem";
+
+interface NavDesktopProps {
+  children?: ReactNode;
+}
+
+export function NavDesktop({ children }: NavDesktopProps) {
+  return (
+    <nav className="bg-white absolute w-full left-0 -bottom-[86px] shadow-custom1 h-16 rounded-[10px] hidden lg:flex lg:items-center lg:justify-between lg:px-[50px]">
+      <ul className="flex gap-x-4">
+        <NavItem text="Home" />
+        <NavItem text="Doctors" />
+        <NavItem text="Departament" />
+        <NavItem text="Services" />
+        <NavItem text="Blog" />
+        <NavItem text="Contact" />
+      </ul>
+
+      <form action="#" className="relative flex gap-x-2">
+        <label
+          htmlFor="search-input"
+          className="flex justify-center items-center"
+        >
+          <i className="ri-search-line text-2xl text-accent-default"></i>
+        </label>
+
+        <input
+          type="text"
+          id="search-input"
+          placeholder="Search..."
+          className="outline-none w-[100px] focus:w-[180px] focus:border-b-2 focus:border-accent-default placeholder:italic placeholder:text-base transition-all duration-150"
+        />
+      </form>
+    </nav>
+  );
+}
---
+++ src/components/NavHiddenMenu/index.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import Image from "next/image";
+import { ReactNode, useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { NavItem } from "../NavItem";
+import logo from "@/assets/img/header/logo.svg";
+
+interface NavHiddenMenuProps {
+  children?: ReactNode;
+}
+
+export function NavHiddenMenu({ children }: NavHiddenMenuProps) {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const [mobileNavClass, setMobileNavClass] = useState("-left-[300px]");
+  const [iconClass, setIconClass] = useState("ri-arrow-right-s-line");
+
+  useEffect(() => {
+    return showNavbar
+      ? setMobileNavClass("left-0")
+      : setMobileNavClass("-left-[300px]");
+  }, [showNavbar]);
+
+  const handleNav = () => {
+    iconClass === "ri-arrow-right-s-line"
+      ? setIconClass("ri-arrow-left-s-line")
+      : setIconClass("ri-arrow-right-s-line");
+
+    return mobileNavClass === "-left-[300px]"
+      ? setShowNavbar(true)
+      : setShowNavbar(false);
+  };
+
+  return (
+    <nav
+      className={twMerge(
+        `mnav bg-white fixed w-[300px] top-0 h-screen ${mobileNavClass} shadow-2xl lg:hidden transition-all duration-300 z-20`
+      )}
+    >
+      <div
+        className="mnav__close-btn bg-primary w-8 h-8 relative -right-full top-8 flex justify-center items-center rounded-tr-lg rounded-br-lg cursor-pointer transition-all"
+        onClick={handleNav}
+      >
+        <i
+          className={twMerge(
+            `mnav__close-btn-icon ${iconClass} text-2xl text-white`
+          )}
+        ></i>
+      </div>
+
+      <div className="px-12 flex flex-col gap-y-12 h-full">
+        <a href="#">
+          <Image src={logo} alt="Logo" priority />
+        </a>
+
+        <ul className="flex flex-col gap-y-5">
+          <NavItem text="Home" />
+          <NavItem text="Doctors" />
+          <NavItem text="Departament" />
+          <NavItem text="Services" />
+          <NavItem text="Blog" />
+          <NavItem text="Contact" />
+        </ul>
+
+        <form action="#" className="relative flex gap-x-[10px]">
+          <label htmlFor="mnav-search-input">
+            <i className="ri-search-line text-2xl text-accent-default"></i>
+          </label>
+
+          <input
+            type="text"
+            id="mnav-search-input"
+            placeholder="Search..."
+            className="outline-none w-[160px] border-b-2 focus:border-b-2 focus:border-accent-default placeholder:italic"
+          />
+        </form>
+      </div>
+    </nav>
+  );
+}
